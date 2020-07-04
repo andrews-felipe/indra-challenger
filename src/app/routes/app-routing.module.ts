85806fd4c3bb071be5ba06b2aa4cbf78c0fd5fb6
Andrew <ferulip@gmail.com>
2020-07-03 23:51:30 -0300

    ajustando detalhes nos arquivos js

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
